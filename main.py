4c2ef1d7f353e08522e2d056145b58adfed474a4
Asta-cmd <jtny026@gmail.com>
2025-06-30 13:20:29 +0800

    main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 import os
 from pyrogram import Client, filters
 
-API_ID = int(os.getenv("API_ID")
+API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_FSUB = os.getenv("CHANNEL_FSUB")
-CHANNEL_TARGET = os.getenv("CHANNEL_TARGET")
 
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
